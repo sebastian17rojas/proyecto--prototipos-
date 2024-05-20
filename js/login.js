b3079659cf5bb3ba6677c44710e5f30e4d1c30a3
sebastian17rojas <negrosebas0209@mail.com>
2024-05-20 10:47:29 -0500

    intenton login

--- js/login.js
+++ js/login.js
@@ -1,11 +1,23 @@
-function login(){
-    let user = document.getElementById("user").value; 
-    let password = document.getElementById("password").value; 
-    
-    if(user === "Brandon" && password === "1234"){
-       window.location = "index.html";
-    } else {
-       alert("Datos incorrectos"); 
-    }
- }
- 
+let Val = document.getElementById("ingresar");
+let user = document.getElementById("user");
+let password = document.getElementById("password");
+
+Val.addEventListener(`click`, function(){
+   if(password.value === ''){
+        
+      console.log('el campo contraseña es obligatorio')
+      evt.preventDefault();
+      return false;
+  
+  }else if(user.value === ''){
+  
+  console.log('el campo de usuario es obligatorio')
+      evt.preventDefault();
+      return false;
+  }
+
+  else if (user.value === "Brandon" && password.value === "1234"){
+    alert("todo correcto")
+      window.location.assign = "../html/index.html";
+  }
+})
