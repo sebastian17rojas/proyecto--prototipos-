91cbdc239c01f5fb8c003326cb059318aafc0248
brandonprogram4 <brandonprogram4@gmail.com>
2024-05-21 21:12:02 -0500

    login funcional

--- js/login.js
+++ js/login.js
@@ -2,22 +2,19 @@ let Val = document.getElementById("ingresar");
 let user = document.getElementById("user");
 let password = document.getElementById("password");
 
-Val.addEventListener(`click`, function(){
-   if(password.value === ''){
-        
-      console.log('el campo contraseña es obligatorio')
-      evt.preventDefault();
-      return false;
+Val.addEventListener(`click`, function(evt){
+    evt.preventDefault(); 
   
-  }else if(user.value === ''){
-  
-  console.log('el campo de usuario es obligatorio')
-      evt.preventDefault();
-      return false;
-  }
-
-  else if (user.value === "Brandon" && password.value === "1234"){
-    alert("todo correcto")
-      window.location.assign = "../html/index.html";
-  }
-})
+    if(password.value === ''){
+        console.log('El campo contraseña es obligatorio');
+        return false;
+    } else if(user.value === ''){
+        console.log('El campo de usuario es obligatorio');
+        return false;
+    } else if (user.value === "Brandon" && password.value === "1234"){
+        window.location.href = "../html/index.html";
+    } else {
+        alert("Usuario o contraseña incorrectos");
+        return false;
+    }
+});
